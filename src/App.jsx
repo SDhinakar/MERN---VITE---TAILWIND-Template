05ee616f42d3015cbdb1e9b9de1404f1bec3264c
Dhinakar S <dhinakarshanmugam6@gmail.com>
2024-09-23 20:03:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='relative h-screen bg-red-500 flex justify-center items-center'>
+      <h1 className='text-center text-white text-3xl'>Start Using the Template</h1>
+    </div>
+  )
+}
+
+export default App
